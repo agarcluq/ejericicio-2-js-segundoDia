aa0ea3ab90d2d64f3748e1369f5b678a1b76fb8f
agarcluq <agarcluq@everis.com>
2020-11-11 10:53:29 +0100

    INICIO

--- index.js
+++ index.js
@@ -1,62 +1,87 @@
-// CONTENIDO DE LA PRÁCTICA:
-// Vamos a añadir elementos en una lista (con la clase "color-list") con javascript a partir del array aportado en este documento, en la constante "colorList" (ver imagen en el proyecto "ejemplo_lista.png"). 
+const listParent = document.querySelector(".color-list");
+const firstChild = listParent.firstChild.nextSibling;
 
-// Como se puede apreciar en la imagen, cada elemento que esté en una posición par de de la lista tiene que tener la clase "color-item--odd". Esta clase debe añadirse desde javascript, NO haciendo uso del selector css nth-of-type(odd) o similares. NOTA: En este caso vamos a considerar un elemento par pensando en el primer elemento como el 1 no como el 0.
+const child2=document.createElement('li');
+const t_child2=document.createTextNode('color:white');
 
-// Cada elemento del listado contendrá:
-//    * El nombre del color.
-//    * Una muestra en la que se ve el color.
-//    * Un botón que modifica el color del siguiente elemento de la lista.
-//    * Un botón que modifica el color del fondo de la página.
-// La información de cada item la obtendremos de cada objeto del array "colorList"
+child2.appendChild(t_child2)
+firstChild.appendChild(child2)
 
-// La estructura de un item de la lista deberá quedar con de la siguiente forma en el HTML (ejemplo del item para el color "white"):
-// <li class="color-item">
-// 	<div class="color-name">Color: white</div>
-// 	<div class="color-show">Muestra</div>
-// 	<button class="color-set">Next item color</button>
-// 	<button class="color-set">Page color</button>
-// </li>
+var dupNode1=firstChild.cloneNode(false);
+listParent.append(dupNode1);
 
-// En esta práctica hay que añadir 4 funcionalidades:
-//    * Al hacer click directamente (no en un item o botón) sobre el fondo de la página (elemento body), debe aparecer un alert en el que aparezca la palabra "body".
-//    * Al hacer click directamente sobre uno de los items de la lista (no en uno de sus botones) debe aparecer un "alert" en el que se indique el nombre de su color.
-//    * Al hacer click sobre el botón con el texto "Next item color" deberá aplicarse el color de ese item al color de fondo del siguiente item (el último item cambia al primero).
-//    * Al hacer click sobre el botón con el texto "Page color" deberá aplicarse el color de ese item al color de fondo de la página (elemento body).
+var dupNode2=firstChild.cloneNode(false);
+listParent.append(dupNode2);
 
-// Buena suerte!
+var dupNode3=firstChild.cloneNode(false);
+listParent.append(dupNode3);
+
+var dupNode4=firstChild.cloneNode(false);
+listParent.append(dupNode4);
+/*
+  let lChild2=document.createElement('li')
+  let lChild3=document.createElement('li')
+  let lChild4=document.createElement('li')
+
+  let s_child2=document.createElement('span')
+  const t_child2=document.createTextNode("color:white");
+  lChild2.appendChild(s_child2)
+  s_child2.appendChild(t_child2)
+  lChild2.classList.add("color-item--odd");
+
+  let s_child3=document.createElement('span')
+  const t_child3=document.createTextNode("color:red");
+  lChild3.appendChild(s_child3)
+  s_child3.appendChild(t_child3)
+  lChild3.classList.add("color-item--odd");
+
+  let s_child4=document.createElement('span')
+  const t_child4=document.createTextNode("color:red");
+  lChild4.appendChild(s_child4)
+  s_child4.appendChild(t_child4)
+  lChild4.classList.add("color-item--odd");
+
+  listParent.appendChild(lChild2);
+  listParent.appendChild(lChild3);
+  listParent.appendChild(lChild4);
+
+
+
+console.log(listParent);
+*/
+
 
 const colorList = [
   {
-    colorName: 'white',
-    hex: '#ffffff'
+    colorName: "white",
+    hex: "#ffffff"
   },
   {
-    colorName: 'red',
-    hex: '#ff0000'
+    colorName: "red",
+    hex: "#ff0000"
   },
   {
-    colorName: 'orange',
-    hex: '#ffa500'
+    colorName: "orange",
+    hex: "#ffa500"
   },
   {
-    colorName: 'yellow',
-    hex: '#ffff00'
+    colorName: "yellow",
+    hex: "#ffff00"
   },
   {
-    colorName: 'orchid',
-    hex: '#da70d6'
+    colorName: "orchid",
+    hex: "#da70d6"
   },
   {
-    colorName: 'pink',
-    hex: '#ffc0cb'
+    colorName: "pink",
+    hex: "#ffc0cb"
   },
   {
-    colorName: 'green',
-    hex: '#008000'
+    colorName: "green",
+    hex: "#008000"
   },
   {
-    colorName: 'silver',
-    hex: '#c0c0c0'
+    colorName: "silver",
+    hex: "#c0c0c0"
   }
 ];
